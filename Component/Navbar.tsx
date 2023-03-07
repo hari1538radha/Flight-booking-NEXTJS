2adb6ce466c08081f2bb1757a784a594a1840325
hari1538radha <hari.r2020ece@sece.ac.in>
2023-03-07 21:21:38 +0530

    feat: navbar completed

--- Component/Navbar.tsx
+++ Component/Navbar.tsx
@@ -4,8 +4,15 @@ import Logo from "../public/Logo.svg";
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="flex h-24 justify-between items-center mx-6">
       <Image alt="hi" src={Logo}></Image>
+      <div className=" font-normal text-lg text-[#7C8DB0]   flex items-center justify-center gap-x-5">
+        <div className="hover:text-[#605DEC]">Flights</div>
+        <div className="hover:text-[#605DEC]">Hotels</div>
+        <div className="hover:text-[#605DEC]">Packages</div>
+        <div className="hover:text-[#605DEC]">Sign in</div>
+        <div className="hover:text-[#605DEC] bg-[#605DEC]  text-white rounded py-3 px-5">Sign up</div>
+      </div>
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import Navbar from "../Component/Navbar";
-
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Navbar />
-    </div>
+    <Head>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet"
+      ></link>
+      <div>
+        <Navbar />
+      </div>
+    </Head>
   );
 }
